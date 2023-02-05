5324420d99b7d9eedf5540a9913b2f1ba66c5ad1
Meda Serenaite <meda.serenaite@gmail.com>
2023-02-04 22:47:43 -0600

    moves inventory list to inventory page; renames list-item component to list

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './global/navigation/navigation.component';
 import { ContentContainerComponent } from './containers/content-container/content-container.component';
-import { ListItemComponent } from './list-item/list-item.component';
+import { ListItemComponent } from './list/list.component';
 import { PrimaryButtonComponent } from './buttons/primary-button/primary-button.component';
 
 
--- frontend/src/app/pages/inventory/inventory.component.ts
+++ frontend/src/app/pages/inventory/inventory.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Instrument } from 'src/app/shared/models/Instrument';
+import { InstrumentService } from 'src/app/services/instrument.service';
 
 @Component({
   selector: 'app-inventory',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent {
+  @Input() instruments: Instrument[] = [];
+  
+  constructor(private foodService: InstrumentService, activatedRoute: ActivatedRoute) {
+    let instrumentObservable:Observable<Instrument[]>;
+    activatedRoute.params.subscribe((params) => {
+        instrumentObservable = foodService.getAll();
 
+        instrumentObservable.subscribe((instrumentsXyz) => {
+          this.instruments = instrumentsXyz;
+          console.log(instrumentsXyz)
+        })
+    })
+  }
 }
