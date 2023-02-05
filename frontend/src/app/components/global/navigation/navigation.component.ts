1ccc2cae7252f042307f1b5a0004b9d92f692d71
Meda Serenaite <meda.serenaite@gmail.com>
2023-02-04 21:33:59 -0600

    nav component & inventory page

---
+++ frontend/src/app/components/global/navigation/navigation.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+  @Input() isMobileMenuOn: boolean = false;
+  menuItems = [
+    {
+      name: 'Schedule',
+      url: '/procedure-schedule'
+    },
+    {
+      name: 'Manage',
+      url: '/manage'
+    },
+    {
+      name: 'Inventory',
+      url: '/all'
+    },
+  ]
+  
+}
